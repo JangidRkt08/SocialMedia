f5f1b345cd51ecc82cee275aa2ced8f577409731
Ravikant Jangid <itsmejangid.rk@gmail.com>
2026-08-12 20:12:54 +0530

    FEAT: Setted Up Project.Db-migration,Server,Docker setup complete

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sikozonpc/social
+
+go 1.26.5
+
+require (
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
---
+++ internal/env/env.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	return val
+}
+
+// string -> Int conversion
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+	return valAsInt
+}
