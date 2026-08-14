a0a3f97613d305cff873d18193e7c14353cabcd6
Ravikant Jangid <itsmejangid.rk@gmail.com>
2026-08-14 15:49:14 +0530

    UPDATED: Made Error Handling more cleaner to use

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, "Server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad Request error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Not Found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusNotFound, "Not Found")
+}
